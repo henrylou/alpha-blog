c909c22da9b5c0ec75ba5a26053ae5d921cfd421
Hengyu Lou <hylou34@gmail.com>
2018-07-06 22:13:36 -0700

    create user and comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-	# validates :title, presence: true, length: { minimum: 3, maximum: 10 }
-	# validates :description, presence: true, length: { minimum: 100, maximum: 300 }
+	validates :title, presence: true, length: { minimum: 3 }
+	validates :description, presence: true, length: { minimum: 10 }
 end
