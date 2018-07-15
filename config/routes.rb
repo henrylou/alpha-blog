85ceb4b85075d3531cba39e714d0198cee690dc6
Hengyu Lou <hylou34@gmail.com>
2018-07-15 00:57:17 -0700

    add sign up UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :comments
-  resources :users
+  resources :users, except: [:new]
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
   root 'pages#home'
   get 'about', to: 'pages#about'
-
+  get 'signup', to: 'users#new'
 end
