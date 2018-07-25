1f3438af1c40eecf23129998ead53704c18a75e0
Hengyu Lou <hylou34@gmail.com>
2018-07-24 18:47:27 -0700

    add categories model, controller, view and automated tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :users, except: [:new]
   resources :articles
+  resources :categories, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
