e34efb99d0d8d901c2fc974b0485f3ed12e5fa84
Hengyu Lou <hylou34@gmail.com>
2018-07-25 15:12:15 -0700

    add article and category association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
 	has_many :articles, dependent: :destroy
-	has_many :comments
 	before_save { self.email = email.downcase }
 	validates :username, presence: true, 
 						uniqueness: { case_sensitive: false },
